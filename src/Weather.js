d74bcf4dd30ad899f7ef08e07ddb05f9f64fb167
MimTohidi <mim.tohidi@gmail.com>
2022-02-21 20:13:01 +0330

    replaced fake data with api data

--- src/Weather.js
+++ src/Weather.js
@@ -1,61 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 import "./Weather.css";
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <div className="container">
-        <div className="hero">
-          <div className="row d-flex justify-content-between mb-5">
-            <div className="col-6 description ">
-              <form className="mb-4">
-                <div className="row">
-                  <div className="col-8">
-                    <input
-                      type="text"
-                      placeholder="Enter a city..."
-                      className="form-control search-input"
-                    />
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function handleResponse(respoonse) {
+    setWeatherData({
+      ready: true,
+      temperature: respoonse.data.main.temp,
+      wind: respoonse.data.wind.speed,
+      city: respoonse.data.name,
+      humidity: respoonse.data.main.humidity,
+      description: respoonse.data.weather[0].description,
+      icon: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
+      date: "Thursday, 17:40",
+      country: respoonse.data.sys.country,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <div className="container">
+          <div className="hero">
+            <div className="row d-flex justify-content-between mb-5">
+              <div className="col-6 description ">
+                <form className="mb-4">
+                  <div className="row">
+                    <div className="col-8">
+                      <input
+                        type="text"
+                        placeholder="Enter a city..."
+                        className="form-control search-input"
+                      />
+                    </div>
+                    <div className="col-4 ">
+                      <input
+                        type="submit"
+                        value="🔍"
+                        className="btn search-btn"
+                      />
+                    </div>
                   </div>
-                  <div className="col-4 ">
-                    <input
-                      type="submit"
-                      value="🔍"
-                      className="btn search-btn"
-                    />
-                  </div>
-                </div>
-              </form>
-              <h1>Ilam, Iran</h1>
-              <hr />
-              <p>
-                Thursday, 17:40
-                <br />
-                Today is mostly cloudy.
-              </p>
-            </div>
-            <div className="col-3 weather-temp">
-              <div className="row">
-                <div className="col-4 ">
-                  <img
-                    src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-                    alt="mostly cloudy"
-                    className="weather-icon"
-                  />
-                </div>
-                <div className="col-7">
-                  <span className="temp">9</span>
-                  <span className="unit">°C</span>
-                </div>
+                </form>
+                <h1>
+                  {weatherData.city}, {weatherData.country}
+                </h1>
+                <hr />
+                <p className="text-capitalize">
+                  {weatherData.date}
+                  <br />
+                  {weatherData.description}.
+                </p>
               </div>
-              <div className="d-flex justify-content-around mt-3">
-                <span className="humidity "> Humidity: 74</span>
-                <span className="wind">Wind: 2km/h</span>
+              <div className="col-3 weather-temp">
+                <div className="row">
+                  <div className="col-4 ">
+                    <img
+                      src={weatherData.icon}
+                      alt={weatherData.description}
+                      className="weather-icon"
+                    />
+                  </div>
+                  <div className="col-7">
+                    <span className="temp">
+                      {Math.round(weatherData.temperature)}
+                    </span>
+                    <span className="unit">°C</span>
+                  </div>
+                </div>
+                <div className="d-flex justify-content-around mt-3">
+                  <span className="humidity ">
+                    {" "}
+                    H: {weatherData.humidity} %
+                  </span>
+                  <span className="wind">
+                    W: {Math.round(weatherData.wind)}km/h
+                  </span>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "066e6846e09ae10d49fa3612a2af48a3";
+
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading";
+  }
 }
