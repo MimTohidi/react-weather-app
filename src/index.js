ccebe3183b52f31eaa391ac58d0903329712de1d
MimTohidi <mim.tohidi@gmail.com>
2022-03-08 21:02:04 +0330

    added footer

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ import reportWebVitals from "./reportWebVitals";
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <p className="text-center footer">
+      This project was coded by{" "}
+      <a href="mailto:mim.tohidi@gmail.com">Mim Tohidi</a> and is open-sourced
+      on <a href="https://github.com/MimTohidi/react-weather-app">GitHub</a>.
+    </p>
   </React.StrictMode>,
   document.getElementById("root")
 );
